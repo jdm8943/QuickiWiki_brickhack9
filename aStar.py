8f481a1fc9ca116eb845e1a5d2f1fd86373ef25d
SohailK <sohail.kumar03@gmail.com>
2023-02-25 17:26:24 -0500

    some changes. pleas help

--- aStar.py
+++ aStar.py
@@ -5,36 +5,51 @@ from heapq import *
 site = pw.Site('en', 'wikipedia')  # The site we want to run our bot on
 site.login()
 
-def a_star(start, end):    # start/end are nodes for a*
+def runAStar(start, end):    # start/end are nodes for a*
     self = set();
+    start = node.Node(start, None)
     start.totalPathWeight = 0      # distance from start    
-    open = [start]
+    open = {start}
     closed = set()
+
     while open:
         curr = heappop(open)    # gets current node by getting max f(n)
-        if curr in closed:      # if curr has already been visited go next
-            continue
         closed.add(curr)        # add curr to closed
-        if curr == end:         # if curr = goal return the path
-            return path         # TODO create func that gets 
+
+        print("CURR: ", curr)
+        
         for neighbor in getNeighbors(curr):
-            neighbor.totalPathWeight = neighbor.parent.totalPathWeight + calculateCost(neighbor)
-            if curr.totalPathWeight < neighbor.totalPathWeight:
-                neighbor.parent = curr
-                neighbor.setF();
-                heappush(open, neighbor)
+            if neighbor.page == end:         # if neighbor = goal, return the path
+                print("WE ARE AT THE END!!!")
+                return getPath(neighbor)
+            if curr in closed:      # if curr has already been visited go next
+                continue
+            else:
+                # neighbor.totalPathWeight = neighbor.parent.totalPathWeight + calculateCost(neighbor)
+                if curr.totalPathWeight < neighbor.totalPathWeight:
+
+                    neighbor.parent = curr
+                    # neighbor.setF();
+                    if(curr.name is "Doom (1993 video game)"):
+                        heappush(open, neighbor)
+    
                 
     # TODO Sol get neighbors from a node and return nodes
-    def getNeighbors(root):
-        neighbors = []
-        links = pw.linkedPages()
-        for aLink in links:
-            neighbors.add(node.Node(aLink, root))
-        return neighbors;
-    
-    # TODO Sol return the cost of a page
-    def calculateCost(node):
-        return 0;
-    
-    def getPath():
-        return 0;
+def getNeighbors(root):
+    neighbors = []
+    links = root.page.linkedPages()
+    for aLink in links:
+        neighbors.append(node.Node(aLink, root))
+    return neighbors;
+
+# TODO Sol return the cost of a page
+def calculateCost(node):
+    return 1;
+
+def getPath(node):
+    totPath = []
+    totPath.append(node)
+    while(node.parent is not None):
+        totPath.append(node.parent)
+        node = node.parent
+    return totPath
--- quikiwiki.py
+++ quikiwiki.py
@@ -6,12 +6,23 @@ class Quikiwiki:
     def __init__(self):
         self.site = pywikibot.Site('en', 'wikipedia')  # The site we want to run our bot on
         self.site.login()
-        self.goalPage = pywikibot.Page(self.site, "Doom_(2016_video_game)")
+        self.startPage = pywikibot.Page(self.site, "Doom (1993 video game)")
+        # self.goalPage = pywikibot.Page(self.site, "Doom_(2016_video_game)")
+        self.goalPage = pywikibot.Page(self.site, "Yahoo! Games")
+        
         self.goalCategories = set(self.goalPage.categories())
 
 if __name__=='__main__':
     qw = Quikiwiki()
     print("RAHHH! quikiwiki running!")
     print(qw.site)
+
     print(qw.goalPage.title())
+    listStar = aStar.runAStar(qw.startPage, qw.goalPage)
+    listStar.reverse()
+    print("ASTAR::::::::")
+    num = 0
+    for i in listStar:
+        print(num, ":", i)
+        num+=1
 
