f6fef0798ca11d969059abdf0e1e53529294a52e
sirpighead <jdm8943@rit.edu>
2023-02-25 14:14:08 -0500

    updates heuristics

--- heuristic.py
+++ heuristic.py
@@ -1,1 +1,20 @@
 import pywikibot
+
+class Heuristics:
+
+    @staticmethod
+    def calculateH():
+        pass
+
+    @staticmethod
+    def checkSameCategory(category1, category2):
+        # print(str(category1) + str(category2))
+        pass
+
+
+if __name__=='__main__':
+    site = pywikibot.Site('en', 'wikipedia')  # The site we want to run our bot on
+    site.login()
+    page = pywikibot.Page(site, "The Worlds of Doctor Who")
+    categories = page.categories()
+    Heuristics.checkSameCategory(next(categories), next(categories))
